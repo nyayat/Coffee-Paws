e8cadd5d9b641a4b7c4cc33dbe14c45a9a858c9a
2-1-1-2 <tanyan.francois@gmail.com>
2024-07-02 12:05:16 +0200

    *Wheel not Wheal

---
+++ Assets/Scripts/WheelButtonController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class WheelButtonController : MonoBehaviour
+{
+	public int id;
+	private Animator anim;
+	public static Image selectedItem;
+	private bool selected = false;
+	public Sprite icon;
+	// Start is called before the first frame update
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+		icon = GetComponent<Sprite>();
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		if (selected)
+		{
+			Debug.Log("Selected");
+			selectedItem.sprite = icon;
+		}
+
+	}
+
+	public void Selected()
+	{
+		selected = true;
+		WheelController.ingredientId = id;
+	}
+
+	public void Deselected()
+	{
+		selected = false;
+		WheelController.ingredientId = 0;
+	}
+
+	public void HoverEnter()
+	{
+		anim.SetBool("Hover", true);
+	}
+
+	public void HoverExit()
+	{
+		anim.SetBool("Hover", false);
+	}
+}
